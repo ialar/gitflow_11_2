d0730443f04e0318aa051e08da50dfde55f047d5
Igor Larionov <jedialar@gmail.com>
2023-06-05 20:27:15 +0300

    add function 2 and docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def turn_to_upper(value: str):
+    """Возвращает строку в верхнем регистре"""
     return value.upper()
+
+
+def turn_to_title(value: str):
+    """Возвращает строку с прописными буквами в начале слов"""
+    return value.title()
